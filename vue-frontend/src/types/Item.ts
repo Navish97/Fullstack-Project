44285cd11b83db929eccb0f6c3e9dc335f2f3079
Navid <Navidm@stud.ntnu.no>
2023-03-18 12:57:23 +0100

    Created item type

---
+++ vue-frontend/src/types/Item.ts
@@ -0,0 +1,14 @@
+export interface Item {
+    id: number;
+    description: string;
+    briefDescription: string;
+    price: number;
+    thumbnail: string;
+    category: string;
+    latitude: number;
+    longitude: number;
+    seller: {
+        name: string;
+        email: string;
+    };
+}
