0eec8ac4d989de3d925d61fdbdd5d41b1a19dd63
Navid <Navidm@stud.ntnu.no>
2023-03-18 22:36:25 +0100

    Simple test controller for testing

---
+++ backend/src/main/java/ntnu/idatt2105/project/backend/Controller/TestController.java
@@ -0,0 +1,13 @@
+package ntnu.idatt2105.project.backend.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
