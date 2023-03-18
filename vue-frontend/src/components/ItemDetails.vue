96cb28c300ca132fea34fc9abef4fe9a8b780779
Navid <Navidm@stud.ntnu.no>
2023-03-18 18:30:36 +0100

    Created ItemDetails component for displaying details of selected item

---
+++ vue-frontend/src/components/ItemDetails.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="item-details">
+    <div class="image-container">
+      <img :src="currentImage" class="item-image" alt="item-image" />
+      <button class="image-btn prev" @click="prevImage">&lt;</button>
+      <button class="image-btn next" @click="nextImage">&gt;</button>
+      <div class="image-index">{{ currentImageIndex + 1 }} / {{ props.item.thumbnail.length }}</div>
+    </div>
+    <div class="item-info">
+      <h2>{{ item.title }}</h2>
+      <h3>Pris: {{ formattedPrice }}</h3>
+      <h4>{{ item.description }}</h4>
+      <p>{{ item.location }}</p>
+      <p>{{ item.contact }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed, ref } from 'vue';
+import { Item } from '@/types/ItemType.ts';
+
+const props = defineProps({
+  item: {
+    type: Object as () => Item,
+    required: true
+  },
+});
+
+const currentImageIndex = ref(0);
+const currentImage = computed(() => props.item.thumbnail[currentImageIndex.value]);
+
+const formattedPrice = computed(() => {
+  return `${props.item.price.toFixed(0)} Kr`;
+});
+
+function prevImage() {
+  if (currentImageIndex.value > 0) {
+    currentImageIndex.value -= 1;
+  }
+  else {
+    currentImageIndex.value = props.item.thumbnail.length - 1;
+  }
+}
+
+function nextImage() {
+  if (currentImageIndex.value < props.item.thumbnail.length - 1) {
+    currentImageIndex.value += 1;
+  }
+  else {
+    currentImageIndex.value = 0;
+  }
+}
+</script>
+
+<style>
+.image-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  margin: 0 auto;
+}
+
+.image-index {
+  position: absolute;
+  max-width: 100%;
+  max-height: 100%;
+  object-fit: contain;
+  bottom: 0;
+  font-size: 18px;
+  text-align: center;
+  padding: 2px 16px 2px 16px;
+  background-color: rgba(0, 0, 0, 0.3);
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+}
+
+.item-image {
+  max-width: 100%;
+  max-height: 50vh;
+}
+
+.image-btn {
+  cursor: pointer;
+  position: absolute;
+  height:100%;
+  width: 40px;
+  background-color: rgba(0, 0, 0, 0.32);
+  color: #fff;
+  border: none;
+  font-size: 50px;
+}
+
+.prev {
+  left: 0;
+}
+
+.next {
+  right: 0;
+}
+</style>
