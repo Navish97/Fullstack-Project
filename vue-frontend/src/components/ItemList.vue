7bdf1559bbb38662833ea1b62006e75898419798
Navid <Navidm@stud.ntnu.no>
2023-03-18 16:12:26 +0100

    Created ItemList component for listing ItemComponent

---
+++ vue-frontend/src/components/ItemList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="item-list">
+    <div v-if="listingType==='list'">
+      <ItemComponent v-for="item in items" :item="item" :key="item.id" listing-type="list" />
+    </div>
+    <div v-else-if="listingType==='thumbnail'" class="item-grid">
+      <ItemComponent v-for="item in items" :item="item" :key="item.id" listing-type="thumbnail" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { ItemType } from '@/types';
+import ItemComponent from '@/components/ItemComponent.vue';
+
+const props = defineProps({
+  items: {
+    type: Array as () => ItemType[],
+    required: true
+  },
+  listingType: {
+    type: String,
+    default: 'thumbnail'
+  }
+});
+</script>
+
+<style scoped>
+.item-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.item-grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: 2rem;
+}
+</style>
