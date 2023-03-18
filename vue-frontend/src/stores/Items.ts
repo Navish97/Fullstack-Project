b4e1556ef407e6300d3a6c370db24bedf0f4c73a
Navid <Navidm@stud.ntnu.no>
2023-03-18 19:56:25 +0100

    Created an Items store for containing items

---
+++ vue-frontend/src/stores/Items.ts
@@ -0,0 +1,86 @@
+import { defineStore } from 'pinia'
+import type {Item} from "@/types/ItemType";
+
+export const useItemStore = defineStore({
+  id: 'item',
+  state: () => ({
+    currentItem: {} as Item,
+    items : [{
+      id: 1,
+      description: 'This is the full description of the item',
+      briefDescription: 'This is a brief description of the item',
+      price: 10.99,
+      thumbnail: ['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg', "https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"],
+      category: 'Electronics',
+      title: 'Landscape Portrait',
+      latitude: 37.7749,
+      longitude: -122.4194,
+      seller: {
+        name: 'John Doe',
+        email: 'johndoe@example.com'
+      }},
+      {
+        id: 2,
+        description: 'This is the full description of the item',
+        briefDescription: 'This is a brief description of the item',
+        price: 10.99,
+        thumbnail: ['https://images.unsplash.com/photo-1500964757637-c85e8a162699?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmVhdXRpZnVsJTIwbGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80'],
+        category: 'Electronics',
+        title: 'Landscape Portrait',
+        latitude: 37.7749,
+        longitude: -122.4194,
+        seller: {
+          name: 'John Doe',
+          email: 'johndoe@example.com'
+        }},
+      {
+        id: 2,
+        description: 'This is the full description of the item',
+        briefDescription: 'This is a brief description of the item',
+        price: 10.99,
+        thumbnail: ['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'],
+        category: 'Electronics',
+        title: 'Landscape Portrait',
+        latitude: 37.7749,
+        longitude: -122.4194,
+        seller: {
+          name: 'John Doe',
+          email: 'johndoe@example.com'
+        }},
+      {
+        id: 2,
+        description: 'This is the full description of the item',
+        briefDescription: 'This is a brief description of the item',
+        price: 10.99,
+        thumbnail: ['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'],
+        category: 'Electronics',
+        title: 'Landscape Portrait',
+        latitude: 37.7749,
+        longitude: -122.4194,
+        seller: {
+          name: 'John Doe',
+          email: 'johndoe@example.com'
+        }},
+      {
+        id: 2,
+        description: 'This is the full description of the item',
+        briefDescription: 'This is a brief description of the item',
+        price: 10.99,
+        thumbnail: ['https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'],
+        category: 'Electronics',
+        title: 'Landscape Portrait',
+        latitude: 37.7749,
+        longitude: -122.4194,
+        seller: {
+          name: 'John Doe',
+          email: 'johndoe@example.com'
+        }
+      }
+   ] as Item[],
+  }),
+  actions: {
+    setCurrentItem(item: Item) {
+      this.currentItem = item;
+    },
+  },
+});
