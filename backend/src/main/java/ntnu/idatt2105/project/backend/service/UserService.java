bdbf2816ef5280d7d730441fec169cc542c485e6
Navid <Navidm@stud.ntnu.no>
2023-03-19 22:39:59 +0100

    Modified UserService to use constructor injection instead of field injection

--- backend/src/main/java/ntnu/idatt2105/project/backend/service/UserService.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/service/UserService.java
@@ -10,8 +10,12 @@ import java.util.Optional;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
     @Autowired
-    private UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
