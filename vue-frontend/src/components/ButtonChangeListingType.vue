5af2ffb4343c80676e5672a6314e50eb097ecdee
Navid <Navidm@stud.ntnu.no>
2023-03-19 12:56:35 +0100

    Created button for changing current listing type

---
+++ vue-frontend/src/components/ButtonChangeListingType.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <button @click="changeListingIndex">{{currentListingType}}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useItemStore } from '@/stores/Items';
+import {computed} from "vue";
+
+const itemStore = useItemStore();
+let listingIndex = 0;
+
+function changeListingIndex() {
+  if (listingIndex === 0) {
+    listingIndex = 1;
+  } else {
+    listingIndex = 0;
+  }
+  itemStore.currentListingType = itemStore.listingTypes[listingIndex];
+}
+
+const currentListingType = computed(() => {
+  return itemStore.currentListingType;
+});
+
+</script>
+
+<style scoped>
+button {
+  width: 150px;
+  background-color: rgba(30, 29, 29, 0.99);
+  color: #ffffff;
+  padding: 12px 24px;
+  border-radius: 4px;
+  border: 1px solid #646464;
+  font-size: 14px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #555;
+}
+</style>
