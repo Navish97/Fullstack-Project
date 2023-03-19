ef4152c7200a41f94a7ef69426e9a1dd359d19ca
Navid <Navidm@stud.ntnu.no>
2023-03-19 13:29:27 +0100

    Updated HomeView to implement change listing type component

--- vue-frontend/src/views/HomeView.vue
+++ vue-frontend/src/views/HomeView.vue
@@ -1,15 +1,29 @@
 <template>
-  <div class="content">
-    <div class="items">
-      <ItemList :items="itemStore.items" :listingType="'list'" />
+  <div class="container">
+    <div class="content">
+      <div class="listing-type">
+        <ListingTypeButton />
+      </div>
+      <div class="items">
+        <ItemList :items="itemStore.items" :listingType="currentListingType"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ItemList from '@/components/ItemList.vue';
+import ListingTypeButton from '@/components/ButtonChangeListingType.vue';
+import {computed} from "vue";
 import { useItemStore } from '@/stores/Items';
 
 const itemStore = useItemStore();
+
+const currentListingType = computed(() => {
+  return itemStore.currentListingType;
+});
 </script>
 
+<style>
+
+</style>
