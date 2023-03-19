f0de587d7cb831b4195f25af2ac6d448583617d8
Navid <Navidm@stud.ntnu.no>
2023-03-19 19:28:13 +0100

    changed controller package name to lowercase

--- backend/src/main/java/ntnu/idatt2105/project/backend/controller/TestController.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/controller/TestController.java
@@ -1,4 +1,4 @@
-package ntnu.idatt2105.project.backend.Controller;
+package ntnu.idatt2105.project.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
