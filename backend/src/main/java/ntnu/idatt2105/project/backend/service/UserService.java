1459d2c022db350612f656a5f4711bf3d589f30c
Navid <Navidm@stud.ntnu.no>
2023-03-19 21:34:37 +0100

    Created UserService class

---
+++ backend/src/main/java/ntnu/idatt2105/project/backend/service/UserService.java
@@ -0,0 +1,31 @@
+package ntnu.idatt2105.project.backend.service;
+
+import ntnu.idatt2105.project.backend.model.User;
+import ntnu.idatt2105.project.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
