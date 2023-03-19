47791347a24c465e2b2a36552e863ced6d5a0eb7
Navid <Navidm@stud.ntnu.no>
2023-03-19 13:28:45 +0100

    Created ButtonChangeListingType component for changing how components are listed

--- vue-frontend/src/components/ButtonChangeListingType.vue
+++ vue-frontend/src/components/ButtonChangeListingType.vue
@@ -27,15 +27,20 @@ const currentListingType = computed(() => {
 </script>
 
 <style scoped>
+
+.container {
+  padding-top: 20px;
+}
 button {
-  width: 150px;
+  width: 100px;
   background-color: rgba(30, 29, 29, 0.99);
   color: #ffffff;
-  padding: 12px 24px;
+  padding: 12px 16px;
   border-radius: 4px;
   border: 1px solid #646464;
   font-size: 14px;
   cursor: pointer;
+  text-align: center;
 }
 
 button:hover {
