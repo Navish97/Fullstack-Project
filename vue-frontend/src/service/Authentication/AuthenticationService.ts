881876238709a99b092ffefef8e331ef6699fc1e
Navid <Navidm@stud.ntnu.no>
2023-03-20 22:21:21 +0100

    Changed AuthenticationService to typescript

--- vue-frontend/src/service/Authentication/AuthenticationService.ts
+++ vue-frontend/src/service/Authentication/AuthenticationService.ts
@@ -1,18 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
+import type { AxiosInstance, AxiosResponse } from 'axios';
+import type {LoginData} from "@/types/LoginData";
+import type {RegisterData} from "@/types/RegisterData";
 
-const authenticateAPI = axios.create({
+
+const authenticateAPI: AxiosInstance = axios.create({
     baseURL: 'http://localhost:8080/api/auth',
     withCredentials: true,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
-})
+});
 
-export const postLogin = (login) => {
-    return authenticateAPI.post('/login', login)
-}
+export const postLogin = (login: LoginData): Promise<AxiosResponse> => {
+    return authenticateAPI.post('/login', login);
+};
 
-export const postRegister = (registerData) => {
+export const postRegister = (registerData: RegisterData): Promise<AxiosResponse> => {
     return authenticateAPI.post('/register', registerData);
 };
