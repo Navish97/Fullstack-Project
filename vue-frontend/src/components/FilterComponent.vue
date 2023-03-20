57ad88dc40f24952c32aee696c98bf775e1e8cc6
daniesky <daniesky@stud.ntnu.no>
2023-03-20 16:55:09 +0100

    filter component styling

--- vue-frontend/src/components/FilterComponent.vue
+++ vue-frontend/src/components/FilterComponent.vue
@@ -1,5 +1,6 @@
 <template>
     <div class = "filter-wrapper">
+        <div class = "header">Filter</div>
         <div class = "price">
             <label>Price</label>
             <div class = "price-input">
@@ -13,10 +14,23 @@
                 </div>
             </div>
         </div>
+        <div class = "condition">
+            <div>Condition</div>
+            <div class = "condition-state">
+                <input type ="checkbox" id="used-box">
+                <label for="used-box">Used</label>
+            </div>
+            <div class = "condition-state">
+                <input type ="checkbox" id="new-box">
+                <label for="new-box">New</label>
+            </div>
+        </div>
+        <button class="apply">Apply</button>
     </div>
 </template>
 
 <script setup lang="ts">
+
 </script>
 
 
@@ -40,5 +54,31 @@
     .price-input input{
         width: 97%;
     }
+    .header{
+        text-align: center;
+        font-weight: bolder;
+    }
+    .condition-state{
+        padding: 5px;
+        align-items: center;
+    }
+    .condition-state input{
+        margin-right: 5px;
+        vertical-align: middle;
+    }
+    .condition-state label{
+        vertical-align: middle;
+    }
+    .apply {
+        border-radius: 4px;
+        border: 1px solid #646464;
+        cursor: pointer;
+        font-size: 14px;
+        padding:5px 8px;
+        margin:5px;
+        margin-top: 0;
+        margin-left: auto;
+    }
+    
 
 </style>
