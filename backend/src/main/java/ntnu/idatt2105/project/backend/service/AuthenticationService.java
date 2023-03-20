601db4815de9823dafd57de0d2ef6814bb3bc5e0
eriskjel <eriskjel@stud.ntnu.no>
2023-03-20 13:41:47 +0100

    created service files for authentication

---
+++ backend/src/main/java/ntnu/idatt2105/project/backend/service/AuthenticationService.java
@@ -0,0 +1,54 @@
+package ntnu.idatt2105.project.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import ntnu.idatt2105.project.backend.authentication.AuthenticationRequest;
+import ntnu.idatt2105.project.backend.authentication.AuthenticationResponse;
+import ntnu.idatt2105.project.backend.authentication.RegisterRequest;
+import ntnu.idatt2105.project.backend.exceptions.UserAlreadyExistsException;
+import ntnu.idatt2105.project.backend.model.Role;
+import ntnu.idatt2105.project.backend.model.User;
+import ntnu.idatt2105.project.backend.repository.UserRepository;
+import org.apache.http.auth.InvalidCredentialsException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthenticationResponse register(RegisterRequest request) throws UserAlreadyExistsException {
+        var user = User.builder()
+                .email(request.getEmail())
+                .name(request.getName())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        System.out.println(user);
+        if (repository.findByEmail(user.getEmail()).isPresent())
+            throw new UserAlreadyExistsException("Email is already in use");
+        repository.save(user);
+        var jwToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwToken).build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) throws InvalidCredentialsException {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = repository.findByEmail(request.getEmail()).orElseThrow(() -> new InvalidCredentialsException("Invalid credentials"));
+        var jwToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwToken).build();
+    }
+
+
+}
