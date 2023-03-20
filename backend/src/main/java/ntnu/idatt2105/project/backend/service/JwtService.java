5cbd1dca1444533fdf2583b9418a6a1e06939b0f
eriskjel <eriskjel@stud.ntnu.no>
2023-03-20 19:26:13 +0100

    fixed secret key local

--- backend/src/main/java/ntnu/idatt2105/project/backend/service/JwtService.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/service/JwtService.java
@@ -31,7 +31,7 @@ public class JwtService {
     // otherwise it is running on server and uses the key from the environment variable stored in server.
     @Autowired
     public JwtService(Environment environment, @Value("${MY_JWT_SECRET_KEY:}") String secretKey) {
-        String LOCAL_SECRET_KEY = "THIS KEY IS ONLY USED LOCALLY";
+        String LOCAL_SECRET_KEY = "THISKEYISONLYUSEDLOCALLYTHISKEYISONLYUSEDLOCALLYTHISKEYISONLYUSEDLOCALLY";
         this.SECRET_KEY = Arrays.asList(environment.getActiveProfiles()).contains("dev") ? LOCAL_SECRET_KEY : secretKey;
     }
 
