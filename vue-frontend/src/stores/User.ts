1c2852958c272920cb48ba6736ad30ea8191a0c7
Navid <Navidm@stud.ntnu.no>
2023-03-20 15:34:12 +0100

    Added method bodies related to bookmark into User store

--- vue-frontend/src/stores/User.ts
+++ vue-frontend/src/stores/User.ts
@@ -1,10 +1,12 @@
 import { defineStore } from 'pinia'
 import type { Bookmark } from "@/types/BookmarkType";
+import type {User} from "@/types/UserType";
+import type {Item} from "@/types/ItemType";
 
 export const useUserStore = defineStore({
     id: 'user',
     state: () => ({
-        loggedInUser: "" as string,
+        loggedInUser: {} as User,
         bookmarks: [] as Bookmark[],
     }),
     persist: {
@@ -17,13 +19,17 @@ export const useUserStore = defineStore({
         getBookmarks: (state) => {
             return state.bookmarks
         },
-        isItemBookmarked: (state) => (itemId: number) => {
-            return state.bookmarks.some(bookmark => bookmark.itemId === itemId);
+        isItemBookmarked: (state) => (item: Item) => {
+                return state.bookmarks.some(bookmark => bookmark.itemId === item.id);
+        },
+        addBookmark: (state) => (item: Item) => {
+            //Axios call for adding bookmark to database
         }
+
     },
     actions: {
-        setLoggedInUser(userEmail: string) {
-            this.loggedInUser = userEmail;
+        setLoggedInUser(user: User) {
+            this.loggedInUser = user;
         },
         setBookmarks(bookmarks: Bookmark[]) {
             this.bookmarks = bookmarks;
