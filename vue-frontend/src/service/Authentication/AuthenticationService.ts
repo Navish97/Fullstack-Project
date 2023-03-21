e4c3eece6ae8560cc30f597525c6109d9ffcf66e
Navid <Navidm@stud.ntnu.no>
2023-03-21 16:31:45 +0100

    Updated AuthenticationService to implement the custom AxiosInstance service

--- vue-frontend/src/service/Authentication/AuthenticationService.ts
+++ vue-frontend/src/service/Authentication/AuthenticationService.ts
@@ -1,39 +1,22 @@
-import axios from 'axios';
-import type { AxiosInstance, AxiosResponse } from 'axios';
+
+import type { AxiosResponse } from 'axios';
 import type {LoginData} from "@/types/LoginData";
 import type {RegisterData} from "@/types/RegisterData";
 import type {User} from "@/types/UserType";
-
-
-const authenticateAPI: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api/auth',
-    withCredentials: true,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
-});
+import axiosInstance from "@/service/AxiosInstance";
+import axios from "axios";
 
 export const postLogin = (login: LoginData): Promise<AxiosResponse> => {
-    return authenticateAPI.post('/login', login);
+    return axiosInstance.post('/api/auth/login', login);
 };
 
 export const postRegister = (registerData: RegisterData): Promise<AxiosResponse> => {
-    return authenticateAPI.post('/register', registerData);
+    return axiosInstance.post('/api/auth/register', registerData);
 };
-
-const userApi: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/api',
-    withCredentials: true,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-    }
-});
 
 export const getUserData = async (): Promise<User | null> => {
     try {
-        const response: AxiosResponse<User> = await userApi.get('/my-profile');
+        const response: AxiosResponse<User> = await axiosInstance.get('/api/my-profile');
         console.log(response)
         const { name, email } = response.data;
         return { id: response.data.id, name, email, roles: response.data.roles };
