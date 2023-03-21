e22f3e48633abe51e9a883fb0923941943b0871a
Navid <Navidm@stud.ntnu.no>
2023-03-21 14:02:43 +0100

    Added findByUser methods to repository classes

--- backend/src/main/java/ntnu/idatt2105/project/backend/repository/BookmarkRepository.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/repository/BookmarkRepository.java
@@ -1,9 +1,14 @@
 package ntnu.idatt2105.project.backend.repository;
 
 import ntnu.idatt2105.project.backend.model.Bookmark;
+import ntnu.idatt2105.project.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    List<Bookmark> findByUser(Optional<User> user);
 }
