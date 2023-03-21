5fe5a84be387b6610431e3f60a2d5b2c38004313
eriskjel <eriskjel@stud.ntnu.no>
2023-03-21 10:17:12 +0100

    allowed requests to items api endpoint

--- backend/src/main/java/ntnu/idatt2105/project/backend/config/SecurityConfig.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .requestMatchers("/api/auth/**")
                 .permitAll()
                 .requestMatchers("/h2-ui/**").permitAll()
+                .requestMatchers("api/items/**").permitAll()
                 .requestMatchers(toH2Console()).permitAll()// Add this line if you want to access H2 console without authentication
                 .anyRequest()
                 .authenticated()
