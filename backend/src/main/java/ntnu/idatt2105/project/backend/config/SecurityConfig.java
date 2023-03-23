7b4238220cb5fb7bbe7491deeef4dba9dcb44376
eriskjel <eriskjel@stud.ntnu.no>
2023-03-23 12:06:17 +0100

    possible fix to profiles and security config

--- backend/src/main/java/ntnu/idatt2105/project/backend/config/SecurityConfig.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package ntnu.idatt2105.project.backend.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,39 +17,67 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.logging.Logger;
 
-//import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
+@Profile("dev")
 public class SecurityConfig {
 
     private final AuthenticationProvider authenticationProvider;
     private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final Environment env;
+    Logger logger = Logger.getLogger(SecurityConfig.class.getName());
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .headers().frameOptions().disable().and()
-            .cors().and()
-            .csrf().disable()
-            .authorizeHttpRequests()
-                .requestMatchers("/api/**").permitAll()
-                .requestMatchers("/h2-ui/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                //.requestMatchers("/my-profile").permitAll()
-                //.requestMatchers(toH2Console()).permitAll()// Add this line if you want to access H2 console without authentication
-                .anyRequest()
-                .authenticated()
-            .and()
-            .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
-            .authenticationProvider(authenticationProvider)
-            .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
-        ;
-        return http.build();
+        if (Arrays.asList(env.getActiveProfiles()).contains("dev")){
+            logger.info("Dev profile is active");
+            http
+                    .headers().frameOptions().disable().and()
+                    .cors().and()
+                    .csrf().disable()
+                    .authorizeHttpRequests()
+                    .requestMatchers("/api/**").permitAll()
+                    .requestMatchers("/h2-ui/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    //.requestMatchers("/my-profile").permitAll()
+                    .requestMatchers(toH2Console()).permitAll()// Add this line if you want to access H2 console without authentication
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .authenticationProvider(authenticationProvider)
+                    .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+            ;
+            return http.build();
+        }
+        else{
+            logger.info("Dev profile is not active");
+            http
+                    .headers().frameOptions().disable().and()
+                    .cors().and()
+                    .csrf().disable()
+                    .authorizeHttpRequests()
+                    .requestMatchers("/api/**").permitAll()
+                    .requestMatchers("/h2-ui/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .authenticationProvider(authenticationProvider)
+                    .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+            ;
+            return http.build();
+        }
+
     }
 
     @Bean
