86e699bd4ba60595993c55c8a5e8a1da74ec6ec2
daniesky <daniesky@stud.ntnu.no>
2023-03-25 14:51:58 +0100

    filter can now filter by category

--- backend/src/main/java/ntnu/idatt2105/project/backend/model/dto/Filter.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/model/dto/Filter.java
@@ -1,63 +1,39 @@
 package ntnu.idatt2105.project.backend.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Class represents a filter used to filter searches in the item repository.
  */
+@Getter
+@Setter
 public class Filter {
     int minPrice;
     int maxPrice;
     boolean usedValue;
     boolean newValue;
+    long category;
 
     /**
      * Default constructor for the Filter class.
+     *
      * @param minPrice
      * @param maxPrice
      * @param usedValue
      * @param newValue
      */
-    public Filter(int minPrice, int maxPrice, boolean usedValue, boolean newValue) {
+    public Filter(int minPrice, int maxPrice, boolean usedValue, boolean newValue, long category) {
         this.minPrice = minPrice;
         this.maxPrice = maxPrice;
         this.usedValue = usedValue;
         this.newValue = newValue;
+        this.category = category;
     }
 
     /**
      * Empty constructor, used by the ObjectMapper.
      */
     public Filter() {
-    }
-
-    public int getMinPrice() {
-        return minPrice;
-    }
-
-    public void setMinPrice(int minPrice) {
-        this.minPrice = minPrice;
-    }
-
-    public int getMaxPrice() {
-        return maxPrice;
-    }
-
-    public void setMaxPrice(int maxPrice) {
-        this.maxPrice = maxPrice;
-    }
-
-    public boolean isUsedValue() {
-        return usedValue;
-    }
-
-    public void setUsedValue(boolean usedValue) {
-        this.usedValue = usedValue;
-    }
-
-    public boolean isNewValue() {
-        return newValue;
-    }
-
-    public void setNewValue(boolean newValue) {
-        this.newValue = newValue;
     }
 }
--- backend/src/main/java/ntnu/idatt2105/project/backend/repository/ItemRepository.java
+++ backend/src/main/java/ntnu/idatt2105/project/backend/repository/ItemRepository.java
@@ -17,8 +17,13 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     @Override
     Optional<Item> findById(Long aLong);
 
-    @Query(value = "SELECT i FROM Item i WHERE (:minPrice IS NULL OR i.price >= :minPrice) AND (:maxPrice IS NULL OR i.price <= :maxPrice)")
-    Page<Item> getItemsByPrice(int minPrice, int maxPrice, final Pageable pageable);
+    @Query(value = "SELECT i FROM Item i \n" +
+            "WHERE (\n" +
+            "    (:minPrice IS NULL AND :maxPrice IS NULL) OR \n" +
+            "    (:minPrice = 0 AND :maxPrice = 0) OR\n" +
+            "    (i.price >= :minPrice AND i.price <= :maxPrice)\n" +
+            ") AND (:category_id = 0 OR i.category.id = :category_id)")
+    Page<Item> getItemsFiltered(int minPrice, int maxPrice, long category_id, final Pageable pageable);
     @Query(value = "SELECT i FROM Item i")
     Page<Item> getItems(final Pageable pageable);
 }
