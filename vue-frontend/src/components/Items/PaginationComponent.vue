54935a38d010cf7d1ba35321da2338c4057af109
daniesky <daniesky@stud.ntnu.no>
2023-03-25 15:54:17 +0100

    10 page display at a time

--- vue-frontend/src/components/Items/PaginationComponent.vue
+++ vue-frontend/src/components/Items/PaginationComponent.vue
@@ -1,19 +1,17 @@
 <template>
-    <div class = "pagination">
-        <a href="#" @click.prevent="emitNavPage(-1)" :class="{ disabled: props.currentPage === 1 }">&lt;&lt; Previous</a>
-        <a 
-        v-for="page in pages" 
-        :key="page" 
-        :href="getPageHref(page)" 
-        @click.prevent="emitLoadPage(page)"
-        :class ="{ 'active': page === props.currentPage }">
-         {{  page }}</a>
-        <a href="#" @click.prevent="emitNavPage(1)" :class="{ disabled: props.currentPage === props.pages.length }"> Next >></a>
-    </div>
+  <div class="pagination">
+    <a href="#" @click.prevent="emitNavPage(-1)" :class="{ disabled: props.currentPage === 1 }">&lt;&lt; Previous</a>
+    <a v-if="props.currentPage > 10" :href="getPageHref(1)" @click.prevent="emitLoadPage(1)">1</a>
+    <a v-if="props.currentPage > 11" @click.prevent="emitLoadPage(props.currentPage - 10)">...</a>
+    <a v-for="page in visiblePages" :key="page" :href="getPageHref(page)" @click.prevent="emitLoadPage(page)" :class="{ active: page === props.currentPage }">{{ page }}</a>
+    <a v-if="props.pages.length > visiblePages[visiblePages.length - 1] + 1" @click.prevent="emitLoadPage(props.currentPage + 10)">...</a>
+    <a v-if="props.pages.length > visiblePages[visiblePages.length - 1]" :href="getPageHref(props.pages.length)" @click.prevent="emitLoadPage(props.pages.length)">{{ props.pages.length }}</a>
+    <a href="#" @click.prevent="emitNavPage(1)" :class="{ disabled: props.currentPage === props.pages.length }">Next >></a>
+  </div>
 </template>
 
-<script setup lang = "ts">
-import { defineProps, defineEmits } from 'vue';
+<script setup lang="ts">
+import { defineProps, defineEmits, computed } from 'vue';
 
 const props = defineProps({
   pages: {
@@ -24,16 +22,16 @@ const props = defineProps({
     type: Number,
     default: 1,
   },
-}
-);
+});
 
 const emit = defineEmits(['load-page', 'previous-page']);
-function getPageHref(page: number): string {
-    return `#page=${page}`;
-  }
 
-function emitLoadPage(page : number){
-    emit('load-page', page);
+function getPageHref(page: number): string {
+  return `#page=${page}`;
+}
+
+function emitLoadPage(page: number) {
+  emit('load-page', page);
 }
 
 function emitNavPage(direction: number) {
@@ -45,29 +43,39 @@ function emitNavPage(direction: number) {
   }
   emit('previous-page', direction);
 }
+
+const visiblePages = computed(() => {
+  const total = props.pages.length;
+  const current = props.currentPage;
+  const last = Math.ceil(total / 10) * 10;
+  const start = Math.max(1, Math.floor((current - 1) / 10) * 10);
+  const end = Math.min(start + 9, last, total);
+  const pages = [];
+  for (let i = start; i <= end; i++) {
+    pages.push(i);
+  }
+  return pages;
+});
+
 </script>
 
 <style scoped>
 .pagination {
-    position: relative;
-  }
- 
-  /* pagination styling */
-  .pagination a {
-    color: black;
-    padding: 10px 18px;
-    text-decoration: none;
-  }
- 
-  /* pagination hover effect on non-active */
-  .pagination a:hover:not(.active) {
-    background-color: #031F3B;
-    color: white;
-  }
- 
-  /* pagination hover effect on active*/
- 
-  .pagination a.active {
+  position: relative;
+}
+
+.pagination a {
+  color: black;
+  padding: 10px 18px;
+  text-decoration: none;
+}
+.pagination a:hover:not(.active) {
+  background-color: #031f3b;
+  color: white;
+}
+
+
+.pagination a.active {
   background-color: cornflowerblue;
   color: white;
 }
@@ -84,5 +92,5 @@ function emitNavPage(direction: number) {
   color: inherit;
   cursor: not-allowed;
 }
- 
+
 </style>
