dbd454274147e59c8000db9ec785fea573f8d062
Navid <Navidm@stud.ntnu.no>
2023-03-25 18:34:51 +0100

    Some cleanup

--- vue-frontend/src/components/__tests__/ChangeListingTypeButton.spec.ts
+++ vue-frontend/src/components/__tests__/ChangeListingTypeButton.spec.ts
@@ -1,46 +1,39 @@
-import {expect, it} from "vitest";
-import {mount} from "@vue/test-utils";
+import { expect, it } from "vitest";
+import { mount } from "@vue/test-utils";
 import ListingTypeButton from "@/components/ButtonChangeListingType.vue";
-import {createPinia} from "pinia";
+import { createPinia } from "pinia";
+import { beforeEach } from 'vitest'
 
-it("renders the button with initial thumbnail configuration", () => {
+let wrapper: any;
+beforeEach(() => {
+
     const pinia = createPinia();
-    const wrapper = mount(ListingTypeButton, {
+
+
+    wrapper = mount(ListingTypeButton, {
         global: {
             plugins: [pinia],
         },
     });
+});
 
-    
+it("renders the button with initial thumbnail configuration", () => {
     const button = wrapper.find(".list-button");
+
     expect(button.text()).toEqual("thumbnail");
 });
 
 it("changes listing type to list when clicked", async () => {
-    const pinia = createPinia();
-    const wrapper = mount(ListingTypeButton, {
-        global: {
-            plugins: [pinia],
-        },
-    });
-
-
     const button = wrapper.find(".list-button");
     await button.trigger("click");
+
     expect(wrapper.vm.currentListingType).toEqual("list");
 });
 
 it("changes back to list when clicked twice", async () => {
-    const pinia = createPinia();
-    const wrapper = mount(ListingTypeButton, {
-        global: {
-            plugins: [pinia],
-        },
-    });
-
-
     const button = wrapper.find(".list-button");
-    await button.trigger("click"); // Change to the last listing type
     await button.trigger("click");
-    expect(wrapper.vm.currentListingType).toEqual("thumbnail"); // Change this to match the first listing type
+    await button.trigger("click");
+
+    expect(wrapper.vm.currentListingType).toEqual("thumbnail");
 });
