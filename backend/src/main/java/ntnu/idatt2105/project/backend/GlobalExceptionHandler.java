7686c710fcbc12683860b77ecec3be2c9b5f8e01
Navid <Navidm@stud.ntnu.no>
2023-03-26 13:49:28 +0200

    Added a global exception handler

---
+++ backend/src/main/java/ntnu/idatt2105/project/backend/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package ntnu.idatt2105.project.backend;
+
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ResponseEntity<String> handleFileSizeLimitExceededException(FileSizeLimitExceededException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("The file size limit has been exceeded: " + ex.getMessage());
+    }
+}
