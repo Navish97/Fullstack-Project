54600c3791e0d8420f07dd5cafcda0ee2025f821
daniesky <daniesky@stud.ntnu.no>
2023-03-27 19:40:44 +0200

    added ChatComponent tests

--- vue-frontend/src/components/Chat/ChatComponent.vue
+++ vue-frontend/src/components/Chat/ChatComponent.vue
@@ -9,7 +9,7 @@
         </div>
       </div>
         <div class="message-bar">
-            <input type = "text" placeholder="Send a message..." v-model="messageInput">
+            <input type = "text" placeholder="Send a message..." v-model="messageInput" id ="messageInput">
             <button class="send-button" @click="sendMessageService()">Send</button>
         </div>
     </div>
