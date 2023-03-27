165e67926a9b5981451a8854580bf24a24fe1900
eriskjel <eriskjel@stud.ntnu.no>
2023-03-27 17:18:55 +0200

    created login E2E test

---
+++ vue-frontend/cypress/e2e/login.spec.ts
@@ -0,0 +1,27 @@
+
+export {}
+
+
+describe('Login', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login')
+    })
+
+    it('Shows an error message for password mismatch', () => {
+        cy.get('#inpEmail').type('erik.skjellevik@lyse.net')
+        cy.get('#inpPassword').type('wrong password')
+        cy.get('#button').click()
+
+        // Assert that an error message is displayed for password mismatch
+        cy.get('#error-message').should('contain.text', 'Email or password is incorrect.')
+    })
+
+    it('Login successful', () => {
+        cy.get('#inpEmail').type('erik.skjellevik@lyse.net')
+        cy.get('#inpPassword').type('1')
+        cy.get('#button').click()
+
+        //assert that the user is redirected to the home page, and that the url does not contain anything after the base url
+        cy.url().should('eq', 'http://localhost:3000/')
+    })
+})
