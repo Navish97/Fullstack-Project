8cba60d8c4540fe5c869b0f2d75e243af0a9abd9
eriskjel <eriskjel@stud.ntnu.no>
2023-03-27 17:39:36 +0200

    added E2E test for changing password when wrong

--- vue-frontend/cypress/e2e/myProfile.spec.ts
+++ vue-frontend/cypress/e2e/myProfile.spec.ts
@@ -38,6 +38,36 @@ describe('Login', () => {
         cy.get(".user-name").should('contain.text', 'Erik')
     })
 
+    it('Cannot change password with wrong old password', () => {
+        cy.visit('http://localhost:3000/my-profile/edit/change-password');
+
+        cy.url().should('eq', 'http://localhost:3000/my-profile/edit/change-password');
+
+        cy.get('#inpOldPassword').type('wrong password');
+        cy.get('#inpNewPassword').type('wrong password');
+
+        cy.window().then((win) => {
+            // Stub the window.alert method
+            cy.stub(win, 'alert').as('alertStub');
+        });
+
+        cy.get('#changePasswordBtn').click();
+
+        cy.get('@alertStub').should('be.calledWith', 'Old password is incorrect. Please try again.');
+    });
+
+    /*it('Can change password', () => {
+        cy.visit('http://localhost:3000/my-profile')
+        cy.get("#myAccountBtn").click(  )
+        cy.url().should('eq', 'http://localhost:3000/my-profile/edit')
+
+        cy.get('#inpName').clear().type('Erik')
+        cy.get("#update").click()
+
+        cy.url().should('eq', 'http://localhost:3000/my-profile')
+        cy.get(".user-name").should('contain.text', 'Erik')
+    })*/
+
 
 
 })
