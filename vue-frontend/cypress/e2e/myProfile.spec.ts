3100dc1a67a1e18b940ee50e42976aafdfbbfee8
eriskjel <eriskjel@stud.ntnu.no>
2023-03-27 17:42:44 +0200

    added E2E test for changing password

--- vue-frontend/cypress/e2e/myProfile.spec.ts
+++ vue-frontend/cypress/e2e/myProfile.spec.ts
@@ -56,17 +56,23 @@ describe('Login', () => {
         cy.get('@alertStub').should('be.calledWith', 'Old password is incorrect. Please try again.');
     });
 
-    /*it('Can change password', () => {
-        cy.visit('http://localhost:3000/my-profile')
-        cy.get("#myAccountBtn").click(  )
-        cy.url().should('eq', 'http://localhost:3000/my-profile/edit')
+    it('Can change password sucessfully', () => {
+        cy.visit('http://localhost:3000/my-profile/edit/change-password');
 
-        cy.get('#inpName').clear().type('Erik')
-        cy.get("#update").click()
+        cy.url().should('eq', 'http://localhost:3000/my-profile/edit/change-password');
 
-        cy.url().should('eq', 'http://localhost:3000/my-profile')
-        cy.get(".user-name").should('contain.text', 'Erik')
-    })*/
+        cy.get('#inpOldPassword').type('1');
+        cy.get('#inpNewPassword').type('1');
+
+        cy.window().then((win) => {
+            // Stub the window.alert method
+            cy.stub(win, 'alert').as('alertStub');
+        });
+
+        cy.get('#changePasswordBtn').click();
+
+        cy.get('@alertStub').should('be.calledWith', 'Password changed successfully!');
+    });
 
 
 
